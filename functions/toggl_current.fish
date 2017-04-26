ec18b5f9e90285b04533ecf29f43b0eda623352a
Katsuma Ito <katsumai@gmail.com>
2017-04-26 18:47:38 +0900

    toggl_current: Make nothing to print when there is no task now

--- functions/toggl_current.fish
+++ functions/toggl_current.fish
@@ -1,5 +1,8 @@
 function toggl_current
   toggl --cache -csv current | read -z current
+  if echo $current | grep 'No time' > /dev/null
+      return
+  end
   echo $current | grep 'Project' | cut -d ',' -f2 | read task_project
   echo $current | grep 'Description' | cut -d ',' -f2 | cut -c 1-20 | read task_desc
   echo $current | grep 'Duration' | cut -d ',' -f2 | read task_duration
