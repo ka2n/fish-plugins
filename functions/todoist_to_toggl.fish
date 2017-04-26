22b3c20dd0be9c288c7701e02658b554289c80d9
Katsuma Ito <katsumai@gmail.com>
2017-04-26 13:12:20 +0900

    refactor todoist_to_toggl.fish

--- functions/todoist_to_toggl.fish
+++ functions/todoist_to_toggl.fish
@@ -3,9 +3,9 @@ function todoist_to_toggl
   if [ ! -n "$item_id" ]
     return 0
   end
-  todoist --csv show $item_id | read item
-  todoist --csv show $item_id | grep 'Project' | cut -d',' -f2 | cut -d':' -f1 | sed 's/^#//' | read project_name
-  todoist --csv show $item_id | grep 'Content' | cut -d',' -f2- |  sed s/\"//g | read task_content
+  todoist --csv show $item_id | read -z item
+  echo $item | grep 'Project' | cut -d',' -f2 | cut -d':' -f1 | sed 's/^#//' | read project_name
+  echo $item | grep 'Content' | cut -d',' -f2- |  sed s/\"//g | read task_content
   toggl --cache projects | grep $project_name | cut -d ' ' -f 1 | read toggl_project_id
   if [ -n "$toggl_project_id" ]
     toggl start -P $toggl_project_id "$task_content"
